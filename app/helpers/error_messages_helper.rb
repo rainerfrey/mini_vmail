a7cb98d89cfbdd33352b948ac5c4df9c8d47f083
Rainer Frey <frey.rainer@gmail.com>
2010-07-22 17:15:23 +0200

    error messages helper: combine edge rails and nifty:layout formatting and use i18n

--- app/helpers/error_messages_helper.rb
+++ app/helpers/error_messages_helper.rb
@@ -2,11 +2,17 @@ module ErrorMessagesHelper
   # Render error messages for the given objects. The :message and :header_message options are allowed.
   def error_messages_for(*objects)
     options = objects.extract_options! 
-    options[:header_message] ||= "Invalid Fields"
-    options[:message] ||= "Correct the following errors and try again."
     messages = objects.compact.map { |o| o.errors.full_messages }.flatten
     unless messages.empty?
-      content_tag(:div, :class => "error_messages") do
+      object = objects[0]
+      model = if object.class.respond_to?(:model_name)
+         object.class.model_name.human
+      else
+        object.class.to_s.humanize
+      end
+      options[:header_message] ||= t(:'activerecord.errors.template.header', :count => messages.count, :model => model)
+      options[:message] ||= t(:'activerecord.errors.template.body')
+      content_tag(:div, :id => "error_messages") do
         list_items = messages.map { |msg| content_tag(:li, msg) }
         content_tag(:h2, options[:header_message]) + content_tag(:p, options[:message]) + content_tag(:ul, list_items.join.html_safe)
       end
