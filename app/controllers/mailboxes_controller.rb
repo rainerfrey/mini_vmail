d8fa075c76fb050a178eec7caf963713a9158352
Rainer Frey <frey.rainer@gmail.com>
2010-07-22 17:21:57 +0200

    add mailboxes

---
+++ app/controllers/mailboxes_controller.rb
@@ -0,0 +1,48 @@
+class MailboxesController < ApplicationController
+  respond_to :html,:xml,:json
+  before_filter :require_user
+  before_filter :set_select_domains, :only => [:new, :create, :edit, :update]
+  
+  def index
+    @mailboxes = Mailbox.ordered
+    respond_with @mailboxes
+  end
+  
+  def show
+    @mailbox = Mailbox.find(params[:id])
+    respond_with @mailbox
+  end
+  
+  def new
+    @mailbox = Mailbox.new
+    
+    respond_with @mailbox
+  end
+  
+  def create
+    @mailbox = Mailbox.new(params[:mailbox])
+    set_modificator @mailbox
+    flash[:notice] = I18n.t(:'resource.created', :model=> Mailbox.model_name.human) if @mailbox.save
+    respond_with @mailbox
+   end
+  
+  def edit
+    @mailbox = Mailbox.find(params[:id])
+    respond_with @mailbox
+  end
+  
+  def update
+    @mailbox = Mailbox.find(params[:id])
+    set_modificator @mailbox
+    flash[:notice] = I18n.t(:'resource.updated', :model=> Mailbox.model_name.human) if @mailbox.update_attributes(params[:mailbox])
+    respond_with @mailbox
+  end
+  
+  def destroy
+    @mailbox = Mailbox.find(params[:id])
+    @mailbox.destroy
+    flash[:notice] = I18n.t(:'resource.destroyed', :model=> Mailbox.model_name.human)
+    respond_with @mailbox
+  end
+  
+end
---
+++ app/models/mailbox.rb
@@ -0,0 +1,22 @@
+class Mailbox < ActiveRecord::Base
+  belongs_to :domain
+  scope :active, where(:active => true)
+  scope :ordered, order(:active)
+  
+  validates_presence_of :name, :password, :domain_id
+  validates_uniqueness_of :name, :scope => :domain_id
+  validates_confirmation_of :my_password
+  
+  attr_protected :password
+  
+  attr_reader :my_password
+  
+  def my_password=(pass)
+    @my_password=pass
+    self.password=pass unless pass.blank?
+  end
+  
+  def to_s
+    domain ? "#{name}@#{domain}" : name
+  end
+end
