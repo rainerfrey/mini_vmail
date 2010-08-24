b83299d63962e131f3de1b7c464edc158ecd152b
Rainer Frey <frey.rainer@gmail.com>
2010-08-24 18:33:05 +0200

    don't use I18n fallback

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,6 @@ MiniVmail::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
-  config.i18n.fallbacks = true
+  config.i18n.fallbacks = false
   config.active_support.deprecation = :notify
 end
