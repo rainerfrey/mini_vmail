41a6f9848f709f88a83f15cc91e3012e3af90307
Rainer Frey <frey.rainer@gmail.com>
2011-12-07 08:59:43 +0100

    upgrade config to rails 3.1

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = '57f7dd0952be02dbb36489f54fd128999b9910030e081472ea9d2acc18dda4c463c8f5de2f27aa856badc779e3704bb74b307b57ecddfb3f7c6ae428848bcad2'
+MiniVmail::Application.config.secret_token = 'c34251ca21b043a017a85bbe8598889cd557db8ad8d0e6a61ff99653a972a6af7a3d5b489256b999f58db621a0de16f2a27b503faa95ded86c6387690fc1d172'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-MiniVmail::Application.config.session_store :cookie_store, :key => '_mini_vmail_session'
+MiniVmail::Application.config.session_store :cookie_store, key: '_mini_vmail_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
