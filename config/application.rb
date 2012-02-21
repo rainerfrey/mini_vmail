878d241dab4a04ba17a90afff3916c41072f9860
Rainer Frey <frey.rainer@gmail.com>
2012-02-21 07:14:06 +0100

    Rails 3.2 config update

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module MiniVmail
     STRING=[MAJOR,MINOR,TINY].join(".")
     DISPLAY="v#{STRING} - #{DATE}"
   end
-  
+
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -47,8 +47,18 @@ module MiniVmail
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-    
+
+    # Use SQL instead of Active Record's schema dumper when creating the database.
+    # This is necessary if your schema can't be completely dumped by the schema dumper,
+    # like if you have constraints or database-specific column types
+    # config.active_record.schema_format = :sql
     config.active_record.timestamped_migrations = false
+    
+    # Enforce whitelist mode for mass assignment.
+    # This will create an empty whitelist of attributes available for mass-assignment for all models
+    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
+    # parameters by using an attr_accessible or attr_protected declaration.
+    # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,29 @@
 MiniVmail::Application.routes.draw do
-  get "downloads" => 'downloads#index' 
-
-  get "downloads/export_relay_domains"
-  get "downloads/virtual_aliases"
-  get "downloads/relay_domains"
-  get "downloads/relay_recipients"
-  get "downloads/dovecot_auth"
-
-  match "login" => 'user_sessions#new', :as => :login
-  match "logout" => 'user_sessions#destroy', :as => :logout
-
-  resource :user_session
-
-  resources :users
-
-  resources :forwards
-
-  resources :mailboxes
-
-  resources :domains
-
-  get "welcome/index"
-
-  get "welcome/initial_login"
-
-  post "welcome/create_initial_login"
-  
-  root :to => "welcome#index"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
+	
+	get "downloads" => 'downloads#index' 
+
+	get "downloads/export_relay_domains"
+	get "downloads/virtual_aliases"
+	get "downloads/relay_domains"
+	get "downloads/relay_recipients"
+	get "downloads/dovecot_auth"
+
+	match "login" => 'user_sessions#new', :as => :login
+	match "logout" => 'user_sessions#destroy', :as => :logout
+
+	resource :user_session
+	resources :users
+	resources :forwards
+	resources :mailboxes
+	resources :domains
+
+	get "welcome/index"
+	get "welcome/initial_login"
+	post "welcome/create_initial_login"
+
+	root :to => "welcome#index"
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -45,12 +39,12 @@ MiniVmail::Application.routes.draw do
   # Sample resource route with options:
   #   resources :products do
   #     member do
-  #       get :short
-  #       post :toggle
+  #       get 'short'
+  #       post 'toggle'
   #     end
   #
   #     collection do
-  #       get :sold
+  #       get 'sold'
   #     end
   #   end
 
@@ -64,7 +58,7 @@ MiniVmail::Application.routes.draw do
   #   resources :products do
   #     resources :comments
   #     resources :sales do
-  #       get :recent, :on => :collection
+  #       get 'recent', :on => :collection
   #     end
   #   end
 
@@ -77,11 +71,11 @@ MiniVmail::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  # match ':controller(/:action(/:id))(.:format)'
 end
